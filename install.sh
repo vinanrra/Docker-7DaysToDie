c25d2582cea2a3698c0be709956ee33754313af5
vinanrra <vicentefernandezguerra@gmail.com>
2019-10-24 00:06:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-./home/sdtdserver/sdtdserver auto-install;
+./sdtdserver auto-install;
 # Update server
-./home/sdtdserver/sdtdserver update
+./sdtdserver update;
 # Update serverconfig
 cp /home/sdtdserver/sdtdserverserverfiles/serverconfig.xml /home/sdtdserver/sdtdserverserverfiles/sdtdserver.xml
 # Start server
-./home/sdtdserver/sdtdserver start
+./sdtdserver start
