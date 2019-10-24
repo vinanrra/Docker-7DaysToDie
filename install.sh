01f7b9ef455975b56f96fc2b2bb37a20e2b2587d
vinanrra <vicentefernandezguerra@gmail.com>
2019-10-24 19:29:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,32 +7,32 @@ fi
 
 # Start server
 if [ "$START_MODE" = "1" ]; then
-	./sdtdserver start
+    ./sdtdserver start
 fi
 
 # Update to stable
 if [ "$START_MODE" = "2" ]; then
-    cp sdtdserver.cfg.stable /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
+    cp sdtdserver.cfg.stable lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update && exit
 fi
 
 # Update to stable and start
 if [ "$START_MODE" = "3" ]; then
-    cp sdtdserver.cfg.stable /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
+    cp sdtdserver.cfg.stable lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
-	./sdtdserver start
+    ./sdtdserver start
 fi
 
 # Update to experimental
 if [ "$START_MODE" = "4" ]; then
-    cp sdtdserver.cfg /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
+    cp sdtdserver.cfg lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update && cp serverfiles/serverconfig.xml serverfiles/sdtdserver.xml && exit
 fi
 
 # Update to experimental and start
 if [ "$START_MODE" = "5" ]; then
-    cp sdtdserver.cfg /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
+    cp sdtdserver.cfg lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
     cp serverfiles/serverconfig.xml serverfiles/sdtdserver.xml
-	./sdtdserver start
+    ./sdtdserver start
 fi
