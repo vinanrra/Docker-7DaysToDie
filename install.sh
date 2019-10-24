7639a29264dbe03cd004475a3fa27cc14f198f35
vinanrra <vicentefernandezguerra@gmail.com>
2019-10-24 15:26:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 if [ "$START_MODE" = "0" ]; then
-	echo "Installing 7 days to die server"
+    echo "Installing 7 days to die server"
     ./sdtdserver auto-install
     echo "7 Days to die server installed"
     exit
 fi
 
 if [ "$START_MODE" = "1" ]; then
-	echo "Starting 7 days to die server"
+    echo "Starting 7 days to die server"
     ./sdtdserver start
     echo "7 Days to die server started"
 fi
 
 if [ "$START_MODE" = "2" ]; then
-	echo "Updating 7 days to die server"
+    echo "Updating 7 days to die server"
     cp sdtdserver.cfg.stable /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
     echo "7 Days to die server updated"
@@ -20,7 +20,7 @@ if [ "$START_MODE" = "2" ]; then
 fi
 
 if [ "$START_MODE" = "3" ]; then
-	echo "Updating 7 days to die server"
+    echo "Updating 7 days to die server"
     cp sdtdserver.cfg.stable /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
     echo "7 Days to die server updated"
@@ -29,7 +29,7 @@ if [ "$START_MODE" = "3" ]; then
 fi
 
 if [ "$START_MODE" = "4" ]; then
-	echo "Updating 7 days to die server to LATEST_EXPERIMENTAL"
+    echo "Updating 7 days to die server to LATEST_EXPERIMENTAL"
     cp sdtdserver.cfg /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
     cp serverfiles/serverconfig.xml serverfiles/sdtdserver.xml
@@ -38,7 +38,7 @@ if [ "$START_MODE" = "4" ]; then
 fi
 
 if [ "$START_MODE" = "5" ]; then
-	echo "Updating 7 days to die server to LATEST_EXPERIMENTAL"
+    echo "Updating 7 days to die server to LATEST_EXPERIMENTAL"
     cp sdtdserver.cfg /home/sdtdserver/lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
     cp serverfiles/serverconfig.xml serverfiles/sdtdserver.xml
