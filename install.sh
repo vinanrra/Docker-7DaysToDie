e5a8c754ef2e38bdba591f40b179da243316306c
vinanrra <vicentefernandezguerra@gmail.com>
2020-02-01 12:14:25 +0100

    Some fixes
    * Fixed script path
    * Improved messages they are now more visual

--- install.sh
+++ install.sh
@@ -10,26 +10,30 @@ if [ "$START_MODE" = "0" ]; then
     # Start to create default files
     ./sdtdserver
     ./sdtdserver auto-install
-	
+
 	echo "
-	
-	IMPORTANT:
-	
-	The server have been installed, now switch between "START_MODE", more info: https://github.com/vinanrra/Docker-7DaysToDie#start-modes
-	
-	"
+        =======================================================================
+        IMPORTANT:
+        
+        The server have been installed, now switch between "START_MODE"
+        More info: https://github.com/vinanrra/Docker-7DaysToDie#start-modes
+        =======================================================================
+        "
 fi
 
 # Start server
 if [ "$START_MODE" = "1" ]; then
-    ./home/stdserver/sdtdserver start
+    ./sdtdserver start
+
+        echo "
+        =======================================================================
+        IMPORTANT:
 	
-	echo "
-	
-	Starting server...
-	Soon you will see all the info
-	"
-	
+        Starting server...
+        Soon you will see all the info
+        =======================================================================
+        "
+
     sleep 2m
     ./sdtdserver details
     tail -f /dev/null
@@ -38,14 +42,18 @@ fi
 # Update to stable
 if [ "$START_MODE" = "2" ]; then
     cp -v sdtdserver.cfg.stable lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
-	
+
     ./sdtdserver update
-	
+
 	echo "
+	=======================================================================
 	IMPORTANT:
-	
-	The server have been updated, now switch between "START_MODE", more info: https://github.com/vinanrra/Docker-7DaysToDie#start-modes"
-	
+
+	The server have been installed, now switch between "START_MODE"
+	More info: https://github.com/vinanrra/Docker-7DaysToDie#start-modes
+	=======================================================================
+	"
+
 	exit
 fi
 
@@ -53,20 +61,24 @@ fi
 if [ "$START_MODE" = "3" ]; then
     cp -v sdtdserver.cfg.stable lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
-	
+
 	echo "Server updated."
-	
+
     ./sdtdserver start
+
+
+        echo "
+        =======================================================================
+        IMPORTANT:
 	
-	echo "
-	
-	Starting server...
-	Soon you will see all the info
-	"
+        Starting server...
+        Soon you will see all the info
+        =======================================================================
+        "
     sleep 2m
     ./sdtdserver details
     tail -f /dev/null
-	
+
 fi
 
 # Update to experimental
@@ -74,11 +86,15 @@ if [ "$START_MODE" = "4" ]; then
     cp -v sdtdserver.cfg lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
     cp -v serverfiles/serverconfig.xml serverfiles/sdtdserver.xml
-	
+
     echo "
-	IMPORTANT:
-	
-	The server have been updated to EXPERIMENTAL, now switch between "START_MODE", more info: https://github.com/vinanrra/Docker-7DaysToDie#start-modes"
+        =======================================================================
+        IMPORTANT:
+
+	The server have been installed, now switch between "START_MODE"
+	More info: https://github.com/vinanrra/Docker-7DaysToDie#start-modes
+	=======================================================================
+	"
 fi
 
 # Update to experimental and start
@@ -86,17 +102,20 @@ if [ "$START_MODE" = "5" ]; then
     cp -v sdtdserver.cfg lgsm/config-lgsm/sdtdserver/sdtdserver.cfg
     ./sdtdserver update
     cp -v serverfiles/serverconfig.xml serverfiles/sdtdserver.xml
-	
+
 	echo "Server updated."
-	
+
     ./sdtdserver start
+
+        echo "
+        =======================================================================
+        IMPORTANT:
 	
-	echo "
-	
-	Starting server...
-	Soon you will see all the info
-	"
-	
+        Starting server...
+        Soon you will see all the info
+        =======================================================================
+        "
+
     sleep 2m
     ./sdtdserver details
     tail -f /dev/null
