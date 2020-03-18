d17f9d2daf09bbce753e413c471cd3ad44d17d22
vinanrra <vicentefernandezguerra@gmail.com>
2020-03-18 13:32:54 +0100

    Update install.sh
    Use of "stable" linuxgsm script for Docker container

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
             =======================================================================
             "
 	
-        wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh sdtdserver
+        wget -O linuxgsm.sh https://raw.githubusercontent.com/vinanrra/Docker-7DaysToDie/master/linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh sdtdserver
 	
     fi
     
