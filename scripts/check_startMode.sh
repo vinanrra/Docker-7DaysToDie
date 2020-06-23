442425bcbde9580512ff3ae5136a8a5b99bce380
vinanrra <vicentefernandezguerra@gmail.com>
2020-06-23 20:27:55 +0200

    Create check_startMode.sh

---
+++ scripts/check_startMode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "
+            =======================================================================
+            IMPORTANT:
+    
+            START_MODE $START_MODE UNKNOWN
+	    
+	          Stopping container...
+    
+            Check your START_MODE, the number must be between 1 and 6
+            More info: https://github.com/vinanrra/Docker-7DaysToDie#start-modes
+            =======================================================================
+            "
