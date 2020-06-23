d59ad76883737f83748de2b59cd519805411b744
vinanrra <vicentefernandezguerra@gmail.com>
2020-06-23 20:20:35 +0200

    Create check_script.sh

---
+++ scripts/check_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "
+            =======================================================================
+            IMPORTANT:
+            
+            LinuxGSM script is missing, downloading...
+            =======================================================================
+            "
+	
+            chmod +x linuxgsm.sh && bash linuxgsm.sh sdtdserver
