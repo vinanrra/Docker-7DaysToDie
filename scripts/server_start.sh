372419b29fcbfaff633e30fad6ed1a9ad14d95e4
vinanrra <vicentefernandezguerra@gmail.com>
2020-06-23 20:22:44 +0200

    Create server_start.sh

---
+++ scripts/server_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+./sdtdserver start
+
+            echo "
+            =======================================================================
+            IMPORTANT:
+            
+            Starting server...
+            Soon you will see all the info
+            =======================================================================
+            "
+
+        sleep 2m
+        
+        ./sdtdserver details
