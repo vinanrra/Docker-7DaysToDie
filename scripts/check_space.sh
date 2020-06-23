a631a6849def6758fe028f56f4f0e4614e4484d6
vinanrra <vicentefernandezguerra@gmail.com>
2020-06-23 19:09:16 +0200

    Update check_space.sh

--- scripts/check_space.sh
+++ scripts/check_space.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
 FREEGB=$(expr $FREE / 1024 / 1024)
 if [[ $FREE -lt 12582912 ]]; then               # 12G = 12*1024*1024k
