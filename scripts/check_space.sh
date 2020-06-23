475780e184964853d9a639a8af47a55aa9be349c
vinanrra <vicentefernandezguerra@gmail.com>
2020-06-23 19:23:44 +0200

    Update check_space.sh

--- scripts/check_space.sh
+++ scripts/check_space.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
-FREEGB=$(expr $FREE / 1024 / 1024)
-if [[ $FREE -lt 12582912 ]]; then               # 12G = 12*1024*1024k
-     echo "
-            =======================================================================
-            ERROR:
-            
-            Not enough space.
-            
-            Needed: 12GB
-            Available: $FREEGB
-            
-            =======================================================================
-            "
+free=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
+freeGB=$(expr $free / 1024 / 1024)
+if [[ $free -lt 12582912 ]]; then               # 12G = 12*1024*1024k
+     space=no
 fi
