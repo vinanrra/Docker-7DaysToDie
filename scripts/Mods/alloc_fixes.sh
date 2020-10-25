1fcdff2bd29f45a9079455f5e704a3f7e6567229
vinanrra <vicentefernandezguerra@gmail.com>
2020-10-25 13:01:46 +0100

    Small fixes
    Replaced printHeader with echo, to avoid install of new package.

--- scripts/Mods/alloc_fixes.sh
+++ scripts/Mods/alloc_fixes.sh
@@ -11,23 +11,23 @@ clear
 
 echo "[Alloc Fixes] Downloading release from $DL_LINK"
 
-printHeader "[Alloc Fixes] Downloading files"
+echo "[Alloc Fixes] Downloading files"
 
 downloadRelease
 
-printHeader "[Alloc Fixes] Extracting files"
+echo "[Alloc Fixes] Extracting files"
 
 mkdir -p allocs-temp
 tar --strip-components=1 -xf allocs.tar.gz -C allocs-temp 
 
-printHeader "[Alloc Fixes] Installing components"
+echo "[Alloc Fixes] Installing components"
 
 rm -rf $1/Allocs*
 mv allocs-temp/* $1
 
-printHeader "[Alloc Fixes] Cleanup"
+echo "[Alloc Fixes] Cleanup"
 
 rm allocs.tar.gz
 rm -rf allocs-temp
 
-printHeader "[Alloc Fixes] Finished! ヽ(´▽\`)/"
+echo "[Alloc Fixes] Finished! ヽ(´▽\`)/"
