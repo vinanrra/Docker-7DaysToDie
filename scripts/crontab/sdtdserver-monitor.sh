b5040a49a15d804a13c43d4cc4bb9a84aacd1ea2
Vicente Fernández Guerra <vicentefernandezguerra@gmail.com>
2021-10-27 23:57:20 +0200

    Update sdtdserver-monitor.sh

--- scripts/crontab/sdtdserver-monitor.sh
+++ scripts/crontab/sdtdserver-monitor.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-FILE="/var/spool/cron/crontabs/sdtdserver"
 STRING="*/5 * * * *  /home/sdtdserver/sdtdserver monitor > /dev/null 2>&1"
 
-if  grep -q "$STRING" "$FILE" ; then
+if  crontab -l | grep -q "$STRING" ; then
     (crontab -l 2>/dev/null; echo "*/5 * * * *  /home/sdtdserver/sdtdserver monitor > /dev/null 2>&1") | crontab -
 fi
 
