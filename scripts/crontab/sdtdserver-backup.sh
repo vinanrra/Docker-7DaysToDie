40b451981f6369c2304124f6b596a40d3c6bb16a
Vicente Fernández Guerra <vicentefernandezguerra@gmail.com>
2021-10-28 00:16:20 +0200

    Update sdtdserver-backup.sh

--- scripts/crontab/sdtdserver-backup.sh
+++ scripts/crontab/sdtdserver-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+FILE="/var/spool/cron/crontabs/sdtdserver"
 STRING="0 5 * * *  /home/sdtdserver/sdtdserver backup > /dev/null 2>&1"
 
-if  crontab -l | grep -q "$STRING" ; then
+if  grep -q "$STRING" "$FILE" ; then
          (crontab -l 2>/dev/null; echo "0 5 * * *  /home/sdtdserver/sdtdserver backup > /dev/null 2>&1") | crontab -
 fi
 
