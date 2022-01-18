4650a93b0d8c527764fd5689a5cebf2c8d2d43fe
vinanrra <4247187+vinanrra@users.noreply.github.com>
2022-01-18 09:54:22 +0100

    Added version support and start server (experimental), the best will be change parameters in linuxgsm configs

--- scripts/Mods/undeadLegacy/undead_legacy.sh
+++ scripts/Mods/undeadLegacy/undead_legacy.sh
@@ -2,7 +2,7 @@
 
 SERVERFILES_FOLDER=/home/sdtdserver/serverfiles
 CONFIG_FILE=/home/sdtdserver/serverfiles/7DaysToDieServer_Data/MonoBleedingEdge/etc/mono/config
-DL_LINK="https://ul.subquake.com/dl/dl.php?v=exp"
+DL_LINK="https://ul.subquake.com/dl/dl.php?v=${UndeadLegacyVersion}"
 
 downloadRelease() {
     curl $DL_LINK -SsL -o undeadlegacy.zip
@@ -21,7 +21,13 @@ unzip undeadlegacy.zip -d undeadlegacy-temp
 
 echo "[Undead Legacy] Installing components"
 
-mv undeadlegacy-temp/UndeadLegacyExperimental-main/* $SERVERFILES_FOLDER
+if [ "${UndeadLegacyVersion,,}" == 'exp'  ]; then
+    mv undeadlegacy-temp/UndeadLegacyExperimental-main/* $SERVERFILES_FOLDER
+fi
+
+if [ "${UndeadLegacyVersion,,}" == 'stable'  ]; then
+    mv undeadlegacy-temp/UndeadLegacy-master/* $SERVERFILES_FOLDER
+fi
 
 echo "[Undead Legacy] Cleanup"
 
@@ -36,4 +42,11 @@ echo "[Undead Legacy] Fixing permissions"
 
 chmod +x $SERVERFILES_FOLDER/run_bepinex_server.sh
 
-echo "[Undead Legacy] Finished! ヽ(´▽\`)/"
+echo "[Undead Legacy] Installed ヽ(´▽\`)/"
+
+# Provisional -> https://github.com/GameServerManagers/LinuxGSM/issues/3754
+echo "[Undead Legacy] Starting the server ヽ(´▽\`)/"
+
+cd $SERVERFILES_FOLDER
+
+bash run_bepinex_server.sh
