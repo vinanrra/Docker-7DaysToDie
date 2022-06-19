45709bdce2e176c0d0413f8ac6cd353c253fb457
Vicente Fernández Guerra <vicentefernandezguerra@gmail.com>
2022-06-20 00:08:52 +0200

    Fix server not updating

--- scripts/server_update.sh
+++ scripts/server_update.sh
@@ -16,7 +16,7 @@ if [ "${VERSION,,}" == 'stable'  ] || [ "${VERSION,,}" == 'public'  ]
     else
         if grep -R "branch" "$LSGMSDTDSERVERCFG"
             then
-                sed -i 's/branch=.*/branch="$VERSION"/' $LSGMSDTDSERVERCFG
+                sed -i "s/branch=".*"/branch="\"${VERSION,,}"\"/" $LSGMSDTDSERVERCFG
             else
                 echo branch='"-beta $VERSION"' >> $LSGMSDTDSERVERCFG
                 echo "[INFO] Selecting 7 days to die ${VERSION,,} version"
