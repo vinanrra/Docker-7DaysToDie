8e27aa7fe574ecd4bc646eabb8d1554b3382aaa9
Vinanrra <vicentefernandezguerra@gmail.com>
2023-04-05 20:18:16 +0200

    fix: Missing bracket

--- scripts/Mods/darkness_fall.sh
+++ scripts/Mods/darkness_fall.sh
@@ -8,20 +8,21 @@ VERSION=${VERSION,,}
 if [ "${DARKNESS_FALL_URL,,}" == "False"  ]; then
     echo "[Darkness Fall] Using custom provided url $DARKNESS_FALL_URL , if this fails check if you can git clone the URL"
 else
-  # Change DL_LINK depending on 7 days to die branch version
-  if [ "${VERSION,,}" == 'stable' ] || [ "${VERSION,,}" == 'public' ]; then
-      DL_LINK="https://dev.azure.com/KhaineUK/_git/DarknessFallsA20"
-  elif [ "${VERSION::7}" == 'alpha20' ]; then
-      DL_LINK="https://dev.azure.com/KhaineUK/_git/DarknessFallsA20"
-  else
-      echo "[Darkness Fall] No version found compatible with version ${VERSION}"
-      echo "[Darkness Fall] If there is a compatible version check -> https://community.7daystodie.com/topic/4941-darkness-falls-they-mostly-come-out-at-night/ and install it manually"
-      echo "[Darkness Fall] Omitting installation"
-  fi
+    # Change DL_LINK depending on 7 days to die branch version
+    if [ "${VERSION,,}" == 'stable' ] || [ "${VERSION,,}" == 'public' ]; then
+        DL_LINK="https://dev.azure.com/KhaineUK/_git/DarknessFallsA20"
+    elif [ "${VERSION::7}" == 'alpha20' ]; then
+        DL_LINK="https://dev.azure.com/KhaineUK/_git/DarknessFallsA20"
+    else
+        echo "[Darkness Fall] No version found compatible with version ${VERSION}"
+        echo "[Darkness Fall] If there is a compatible version check -> https://community.7daystodie.com/topic/4941-darkness-falls-they-mostly-come-out-at-night/ and install it manually"
+        echo "[Darkness Fall] Omitting installation"
+    fi
 fi
 
 downloadRelease() {
     git clone $DARKNESS_FALL_URL darknessFall-temp/
+}
 
 echo "[Darkness Fall] Downloading release from $DARKNESS_FALL_URL"
 
