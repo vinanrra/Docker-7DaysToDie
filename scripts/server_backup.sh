8c22069c89eeedc527df835ba2b516829a9d9346
Vinanrra <vicentefernandezguerra@gmail.com>
2024-06-30 11:34:27 +0200

    fix: Correct timestamp for backups

--- scripts/server_backup.sh
+++ scripts/server_backup.sh
@@ -14,7 +14,7 @@ crontab -r
 
 # Backup the specified folder
 echo "Backing up folder: $BACKUP_FOLDER"
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+TIMESTAMP=$(date +"%Y-%m-%d-%H%M%S")
 BACKUP_FILE="$BACKUP_DESTINATION/sdtdserver-$TIMESTAMP.tar.gz"
 tar -czvf $BACKUP_FILE $BACKUP_FOLDER
 
