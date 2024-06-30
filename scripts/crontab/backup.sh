38369a3af42f8234f80a70821d05cdc1b405df0d
Vinanrra <vicentefernandezguerra@gmail.com>
2024-06-30 11:31:08 +0200

    fix: Show log at console

--- scripts/crontab/backup.sh
+++ scripts/crontab/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "0 $BACKUP_HOUR * * *  /home/sdtdserver/scripts/server_backup.sh > /dev/null 2>&1" >> crontab.txt
+echo "0 $BACKUP_HOUR * * *  /home/sdtdserver/scripts/server_backup.sh >> /home/sdtdserver/log/sdtdserver-console.log 2>&1" >> crontab.txt
 
-echo "[INFO] Activated automatic backup at 5AM"
+echo "[INFO] Activated automatic backup at $BACKUP_HOUR"
