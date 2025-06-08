cb95971bceb32986d7cdfc85ea0f09ff32473929
Vicente Fernández Guerra <vicentefernandezguerra@gmail.com>
2025-06-08 13:32:18 +0200

    chore: Add 1.4 Darknessfalls version
    Fix #300

--- scripts/Mods/darkness_falls.sh
+++ scripts/Mods/darkness_falls.sh
@@ -2,7 +2,7 @@
 
 SERVER_FOLDER=/home/sdtdserver/serverfiles
 VERSION=${VERSION,,}
-
+DF_CONFIG="DarknessFallsServerConfig.xml"
 
 # Change DL_LINK depending on 7 days to die branch version
 if [ "${DARKNESS_FALLS_URL}" != "False"  ]; then
@@ -15,9 +15,10 @@ else
         echo "[Darkness Falls] If there is a compatible version check -> https://community.7daystodie.com/topic/4941-darkness-falls-they-mostly-come-out-at-night/ and install it manually or using DARKNESS_FALLS_URL"
         echo "[Darkness Falls] Omitting installation"
         exit
+    elif [ "${VERSION::7}" == '1.4' ]; then
+        DL_LINK="https://dev.azure.com/KhaineUK/_git/Darkness%20Falls%20V6"
     elif [ "${VERSION::7}" == 'alpha21' ]; then
         DL_LINK="https://dev.azure.com/KhaineUK/_git/DarknessFallsA21"
-        DF_CONFIG="DarknessFallsServerConfig.xml"
     elif [ "${VERSION::7}" == 'alpha20' ]; then
         DL_LINK="https://dev.azure.com/KhaineUK/_git/DarknessFallsA20"
         DF_CONFIG="DarknessFallsConfig.xml"
